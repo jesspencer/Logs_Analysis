0cd7e3fb14e35d559a877359e63a392c0e55ab4f
Jessica-Ee-Spencer <jessicaspencer12@gmail.com>
2019-01-26 09:46:46 -0500

    updated script to meet pep8

--- la.py
+++ la.py
@@ -7,7 +7,7 @@ import psycopg2
 
 DBNAME = "news"
 
-'#querying the database' 
+'#querying the database'
 
 query_1 = 'select * from top_three_articles;'
 
@@ -19,7 +19,7 @@ select r.date from requests r join errors e on r.date
 = e.date where e.http_404::numeric/r.http_requests * 100 > 1
 group by r.date, e.http_404, r.http_requests;"""
 
-'#answering questions about data' 
+'#answering questions about data'
 
 ques1 = 'Ques 1: What are the most popular three articles of all time?'
 ques2 = 'Ques 2: Who are the most popular article authors of all time?'
@@ -30,12 +30,12 @@ ques3 = 'Ques 3: On which days did more than 1% of requests lead to errors?'
 if __name__ == '__main__':
     conn = psycopg2.connect(database=DBNAME)
     curr = conn.cursor()
-    
+
     curr.execute(query_1)
     res = curr.fetchall()
     print ques1
     for i in range(len(res)):
-        print i+1, ')', res[i][0], '--', res[i][2] 
+        print i+1, ')', res[i][0], '--', res[i][2]
     print''
 
     curr.execute(query_2)
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     for i in range(len(res)):
         print i+1, ')', res[i][0], '--', res[i][1]
     print ''
-    
+
     curr.execute(query_3)
     res = curr.fetchall()
     print ques3
